da8a2264f7e9d522f98487f92880f27a0c386c4b
RedBlaze42 <allairehugo@gmail.com>
2021-09-25 10:13:12 +0200

    Added cost support to covoit

--- datastores.py
+++ datastores.py
@@ -8,6 +8,7 @@ class Covoit():
         self.is_driver = is_driver
         self.capacity = capacity
         self.destination = destination
+        self.trip_cost = None
         
         if self.is_driver:
             self.passenger_names = list()
@@ -15,12 +16,12 @@ class Covoit():
         else:
             self.driver_name = None
         
-    def calculate_trip(self, matrix, passengers = None, key = "duration"):
+    def calculate_trip(self, matrix, passengers = None, key = "duration", start_step = None):
         trip_sum = 0
         
         if passengers is None: passengers = self.passenger_names
         
-        previous_step = self.name
+        previous_step = self.name if start_step is None else start_step
         for passenger in passengers:
             trip_sum += matrix[previous_step][passenger][key]
             previous_step = passenger
@@ -40,3 +41,13 @@ class Covoit():
             temp_passengers.remove(passenger_name)
             
             return self.calculate_trip(matrix, key = key) - self.calculate_trip(matrix, passengers = temp_passengers)
+
+    def set_trip_costs(self, covoits, matrix, total_cost):
+        if not self.is_driver: return None
+
+        total_distance = sum([matrix[passenger_name]["__end__"]["distance"] for passenger_name in self.passenger_names])
+        total_distance += matrix[self.name]["__end__"]["distance"]
+        
+        self.trip_cost = (matrix[self.name]["__end__"]["distance"] / total_distance) * total_cost
+        for passenger_name in self.passenger_names:
+                covoits[passenger_name].trip_cost = (matrix[passenger_name]["__end__"]["distance"] / total_distance) * total_cost
