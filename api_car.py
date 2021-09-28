4fb643b27b9f69d3b92ce2b04aea77a8bd3ca594
RedBlaze42 <allairehugo@gmail.com>
2021-09-28 22:48:28 +0200

    Refactor Removed directions of michelin Refactored trip_directions of michelin in directions

--- api_car.py
+++ api_car.py
@@ -39,33 +39,7 @@ class Michelin():
         
         return req_data["locationList"]
 
-    def directions(self, orig_id, dest_id, fuel_cost = 1.5):
-        url = "https://secure-apir.viamichelin.com/apir/2/iti.json/fra"
-        data = {
-            "steps":"3:e:{};3:e:{};".format(orig_id, dest_id),
-            "fuelCost": fuel_cost, "fuelConsump":"7.9:6.9:7", "authKey": self.auth_key,
-            "distUnit":"m","veht":0,"itit":0,"avoidClosedRoad":False,"currency":"EUR","indemnite":0,"favMotorways":True,"avoidBorders":False,"avoidTolls":False,"avoidCCZ":False,"avoidORC":False,"avoidPass":False,"wCaravan":False,"withSecurityAdv":True,"avoidExpressWays":False,"fullMapOpt":"300:300:true:true:true","stepMapOpt":"300:300:true:true:true","multipleIti":True,"traffic":"FRA","obfuscation":False,"ie":"UTF-8","charset":"UTF-8","lg":"fra","nocache":time.time()*1000,"protocol":"https", "callback":"JSE.HTTP.asyncRequests[9].HTTPResponseLoaded"
-        }
-
-        req = requests.get(url, params = data)
-        req.raise_for_status()
-        data = req.json()
-        
-        if not "header" in data.keys():
-            print(data)
-            return None
-        
-        output = {
-            "fuel_cost": round(data["header"]["summaries"][0]["consumption"], 2),
-            "toll_cost": round(data["header"]["summaries"][0]["tollCost"]["car"]/100, 2),
-            "total_cost": round(data["header"]["summaries"][0]["consumption"] + data["header"]["summaries"][0]["tollCost"]["car"]/100, 2),
-            "distance_km": int(data["header"]["summaries"][0]["drivingDist"]/1000),
-            "time":data["header"]["summaries"][0]["drivingTime"]
-        }
-
-        return output
-
-    def trip_directions(self, waypoints, fuel_cost = 1.5):
+    def directions(self, waypoints, fuel_cost = 1.5):
         url = "https://vmrest.viamichelin.com/apir/11/iti.json/eng/geom;header;roadsheet"
         data = {
             "stepList": "".join(["1:e:{}:{};".format(waypoint[1], waypoint[0]) for waypoint in waypoints]),
